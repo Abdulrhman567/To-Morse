ccfa281eb9c85193a22057dcbf66e1eaa7e0b6cf
Abdulrhman <ashoradooji7890@gmail.com>
2021-12-04 00:09:54 +0200

    Some Changes On Main

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 def home():
     full_morse_code = ""
     if request.method == 'POST':
-        text_data = request.form.get('usertext').lower()
-        for char in text_data:
-            if char in codes:
-                full_morse_code += ' '.join(codes[char])
-            else:
-                flash("Can't translate these characters, Please try again!", category='error')
-                return redirect(url_for('home'))
+        text_data = request.form.get('usertext')
+        morse_code = [' '.join(codes[char]) if char in codes else 'flash' for char in text_data.lower()]
+        if 'flash' in morse_code:
+            flash("Can't translate these characters, Please try again!", category="error")
+            return redirect(url_for("home"))
+        else:
+            full_morse_code = ''.join(morse_code)
     return render_template('index.html', full_code=full_morse_code)
 
 
